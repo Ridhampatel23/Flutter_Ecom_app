88f379ce81ee29c81d494e96e1cb355f5fb04cea
Ridham Patel <Ridhamsangani.23@gmail.com>
2024-09-16 22:30:25 -0700

    FIRST COMMIT

---
+++ lib/utils/theme/custom_themes/elevated_button_theme.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+
+class ecomElevatedButtonTheme{
+  ecomElevatedButtonTheme._(); //._ makes it static to avoid creating more instances.
+
+  static final LightElevatedButton = ElevatedButtonThemeData(
+    style: ElevatedButton.styleFrom(
+      elevation: 0,
+      foregroundColor: Colors.white,
+      backgroundColor: Colors.blue,
+      disabledForegroundColor: Colors.grey,
+      disabledBackgroundColor: Colors.grey,
+      side: const BorderSide(color: Colors.blue),
+      padding: const EdgeInsets.symmetric(vertical: 10),
+      textStyle: const TextStyle(fontSize: 16, color: Colors.white, fontWeight: FontWeight.w600),
+      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(12)),
+    ),
+  );
+
+  static final darkElevatedButton = ElevatedButtonThemeData(
+    style: ElevatedButton.styleFrom(
+      elevation: 0,
+      foregroundColor: Colors.white,
+      backgroundColor: Colors.blue,
+      disabledForegroundColor: Colors.grey,
+      disabledBackgroundColor: Colors.grey,
+      side: const BorderSide(color: Colors.blue),
+      padding: const EdgeInsets.symmetric(vertical: 10),
+      textStyle: const TextStyle(fontSize: 16, color: Colors.white, fontWeight: FontWeight.w600),
+      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(12)),
+    ),
+  );
+}
