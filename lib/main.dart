64a51477392b23398b905fd98d0083d035137194
Ridham Patel <Ridhamsangani.23@gmail.com>
2024-11-13 20:55:08 -0800

    Few Tweaks after setting up Firebase  (Non - Backend) [The Frontend part is now over, time to move on to Backend!]

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,21 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:get/get_core/src/get_main.dart';
+import 'package:http/http.dart';
 
 import 'app.dart';
+import 'firebase_options.dart';
 
 /// -------Entry Point for the Flutter Application ---------
-void main() {
-  /* TODO : Add Widget Bindings
-  TODO : Init Local Storage
-  TODO : Await Native Splash
-  TODO : Initialize Firebase
-  TODO : Initialize Authentication
-  */
+Future<void> main() async {
+  //  TODO : Add Widget Bindings
+  // TODO : Init Local Storage
+  // TODO : Await Native Splash
+  // TODO : Initialize Firebase
+  //await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform).then((FirebaseApp value) => Get.put(AuthenticationRepository()),);
+  // TODO : Initialize Authentication
 
 
   runApp(const Main_app());
 }
-
-
-
