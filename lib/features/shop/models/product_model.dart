fd6eeaae74b46852cf1a65f22e0f20eaf3cca15f
Ridham Patel <Ridhamsangani.23@gmail.com>
2025-01-07 01:07:45 -0800

    Backend : Added new assets and model repos for Products and their details 2 / 3

--- lib/features/shop/models/product_model.dart
+++ lib/features/shop/models/product_model.dart
@@ -1,3 +1,10 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:ecom_store/features/shop/models/product_attribute_model.dart';
+import 'package:ecom_store/features/shop/models/product_vairation_model.dart';
+import 'package:flutter/foundation.dart';
+
+import 'brand_model.dart';
+
 class ProductModel {
   String id;
   int stock;
@@ -16,6 +23,96 @@ class ProductModel {
   List<ProductAttributeModel>? productAttributes;
   List<ProductVariationModel>? productVariations;
 
-  ProductModel()
-}
+  ProductModel(
+      {required this.id,
+      required this.title,
+      required this.stock,
+      required this.price,
+      required this.thumbnail,
+      required this.productType,
+      this.isFeatured,
+      this.sku,
+      this.brand,
+      this.date,
+      this.images,
+      this.salePrice = 0.0,
+      this.categoryId,
+      this.description,
+      this.productAttributes,
+      this.productVariations});
 
+  /// Empty Model
+  static ProductModel empty() => ProductModel(
+      id: '', title: '', stock: 0, price: 0, thumbnail: '', productType: '');
+
+  /// Json Format
+  toJson() {
+    return {
+      'SKU': sku,
+      'Title': title,
+      'Stock': stock,
+      'Price': price,
+      'Images': images ?? [],
+      'Thumbnail': thumbnail,
+      'SalePrice': salePrice,
+      'IsFeatured': isFeatured,
+      'CategoryId': categoryId,
+      'Brand': brand,
+      'Description': description,
+      'ProductType': productType,
+      'ProductAttributes': productAttributes != null
+          ? productAttributes!.map((e) => e.toJson()).toList()
+          : [],
+      'ProductVariations': productVariations != null
+          ? productVariations!.map((e) => e.toJson()).toList()
+          : [],
+    };
+  }
+
+  /// Map JSON document snapshot from Firebase to Model
+  factory ProductModel.fromSnapshot(
+      DocumentSnapshot<Map<String, dynamic>> document) {
+    final data = document.data()!;
+    return ProductModel(
+      id: document.id,
+      sku: data['SKU'],
+      title: data['Title'],
+      stock: data['Stock'] ?? 0,
+      isFeatured: data['IsFeatured'] ?? false,
+      price: double.parse((data['Price'] ?? 0).toString()),
+      salePrice: double.parse((data['SalePrice'] ?? 0).toString()),
+      thumbnail: data['Thumbnail'] ?? '',
+      categoryId: data['CategoryId'] ?? '',
+      description: data['Description'] ?? '',
+      productType: data['ProductType'] ?? '',
+      brand:  BrandModel.fromJson(data['Brand']),
+      images: data['Images'] != null ? List<String>.from(data['Images']) : [],
+      productAttributes: (data['ProductAttributes'] as List<dynamic>).map((e) => ProductAttributeModel.fromJson(e)).toList(),
+      productVariations: (data['ProductVariations'] as List<dynamic>).map((e) => ProductVariationModel.fromJson(e)).toList(),
+    );
+  }
+
+
+  /// Map JSON document snapshot from Firebase to Model
+  factory ProductModel.fromQuerySnapshot(
+      QueryDocumentSnapshot<Object> document) {
+    final data = document.data as Map<String,dynamic>;
+    return ProductModel(
+      id: document.id,
+      sku: data['SKU'],
+      title: data['Title'],
+      stock: data['Stock'] ?? 0,
+      isFeatured: data['IsFeatured'] ?? false,
+      price: double.parse((data['Price'] ?? 0).toString()),
+      salePrice: double.parse((data['SalePrice'] ?? 0).toString()),
+      thumbnail: data['Thumbnail'] ?? '',
+      categoryId: data['CategoryId'] ?? '',
+      description: data['Description'] ?? '',
+      productType: data['ProductType'] ?? '',
+      brand:  BrandModel.fromJson(data['Brand']),
+      images: data['Images'] != null ? List<String>.from(data['Images']) : [],
+      productAttributes: (data['ProductAttributes'] as List<dynamic>).map((e) => ProductAttributeModel.fromJson(e)).toList(),
+      productVariations: (data['ProductVariations'] as List<dynamic>).map((e) => ProductVariationModel.fromJson(e)).toList(),
+    );
+  }
+}
--- lib/features/shop/models/product_vairation_model.dart
+++ lib/features/shop/models/product_vairation_model.dart
@@ -1,5 +1,4 @@
-
-class ProductVariationModel{
+class ProductVariationModel {
   final String id;
   String sku;
   String image;
@@ -9,7 +8,6 @@ class ProductVariationModel{
   int stock;
   Map<String, String> attributeValues;
 
-
   ProductVariationModel({
     required this.id,
     this.sku = '',
@@ -17,24 +15,40 @@ class ProductVariationModel{
     this.description = '',
     this.price = 0.0,
     this.salePrice = 0.0,
-    this .stock = 0,
+    this.stock = 0,
     required this.attributeValues,
-});
+  });
 
   /// Create Empty func for clean code
-  static ProductVariationModel empty() => ProductVariationModel(id: '', attributeValues: {});
+  static ProductVariationModel empty() =>
+      ProductVariationModel(id: '', attributeValues: {});
 
   /// Json Format
- toJson(){
-   return{
-     'Id' : id,
-     'Image' : image,
-     'Description' : description,
-     'SKU' : sku,
-     'Price' : price,
-     'SalePrice' : salePrice,
-     'Stock' : stock,
-     'AttributeValue' : attributeValues,
-   };
- }
+  toJson() {
+    return {
+      'Id': id,
+      'Image': image,
+      'Description': description,
+      'SKU': sku,
+      'Price': price,
+      'SalePrice': salePrice,
+      'Stock': stock,
+      'AttributeValue': attributeValues,
+    };
+  }
+
+  ///Map JSON oriented document snapshot from Firebase to Model
+  factory ProductVariationModel.fromJson(Map<String, dynamic> document) {
+    final data = document;
+    if (data.isEmpty) return ProductVariationModel.empty();
+    return ProductVariationModel(
+      id: data['Id'],
+      price: double.parse((data['Price'] ?? 0.0).toString()),
+      sku: data['SKU'] ?? '',
+      stock: data['Stock'] ?? 0,
+      salePrice: double.parse((data['SalePrice'] ?? 0.0).toString()),
+      image: data['Image'] ?? '',
+      attributeValues: Map<String, String>.from(data['AttributeValues']),
+    );
+  }
 }
