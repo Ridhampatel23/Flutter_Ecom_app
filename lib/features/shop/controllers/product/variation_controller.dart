4908b848f5cb5061e2410d21f0248069752c83c5
Ridham Patel <Ridhamsangani.23@gmail.com>
2025-01-28 00:51:43 -0800

    Backend : Implemented Product Variation controller and update Product details screen to generate data more dynamically

--- lib/features/shop/controllers/product/variation_controller.dart
+++ lib/features/shop/controllers/product/variation_controller.dart
@@ -1,4 +1,5 @@
 
+import 'package:ecom_store/features/shop/controllers/product/image_controller.dart';
 import 'package:ecom_store/features/shop/models/product_model.dart';
 import 'package:ecom_store/features/shop/models/product_vairation_model.dart';
 import 'package:get/get.dart';
@@ -18,28 +19,59 @@ class VariationController extends GetxController{
     final selectedAttributes = Map<String, dynamic>.from(this.selectedAttributes);
     selectedAttributes[attributeName] = attributeValue;
     this.selectedAttributes[attributeName] = attributeValue;
+    
+    final selectedVariation = product.productVariations!.firstWhere((variation) => _isSameAttributeValue(variation.attributeValues, selectedAttributes),
+      orElse: () => ProductVariationModel.empty(),
+    );
 
+    // Show the Selected Variation image as a Main Image
+    if(selectedVariation.image.isNotEmpty){
+      ImageController.instance.selectedProductImage.value = selectedVariation.image;
+    }
+
+    // Assign the selected Variation
+    this.selectedVariation.value =  selectedVariation;
+
+    // Update selected product variation status
+    getProductVariationStockStatus();
 
   }
 
   /// -- Check if selected attributes matches any variation attributes
-  bool _isSameAttributeValue(Map<String, dynamic> variationAttributes, Map<String, dynamic> selectedAttributes){
+  bool _isSameAttributeValue(Map<String, dynamic> variationAttributes, Map<String, dynamic> selectedAttributes) {
     // If selectedAttributes contain 3 attribtues and current variation contains 2 then return
     if (variationAttributes.length != selectedAttributes.length) return false;
 
     // If any of the attributes is different then return. e.g. [Red, Large] x [Red, Small]
     for (final key in variationAttributes.keys) {
       // Attributes[Key] = Value which could be [Green, Small, Cotton] etc.
-      if(variationAttributes[key] != selectedAttributes[key]) return false;
+      if (variationAttributes[key] != selectedAttributes[key]) return false;
     }
+
+    return true;
   }
 
 
   /// -- Check Attribute availability / Stock in Variation
-  Set<String> getAttributeAvailabilityInVariation(List<ProductVariationModel> variations, String attributeName){}
+  Set<String?> getAttributeAvailabilityInVariation(List<ProductVariationModel> variations, String attributeName){
+    // Pass the variations to check which attributes are available and stock is not 0
+    final availableVariationAttributeValues = variations
+        .where((variation) =>
+      // Check Empty / Out of Stock Attributes
+      variation.attributeValues[attributeName] != null && variation.attributeValues[attributeName]!.isNotEmpty && variation.stock > 0).
+    map((variation) => variation.attributeValues[attributeName])
+    .toSet();
+
+    return availableVariationAttributeValues;
+
+  }
+
+  String getVariationPrice(){
+    return (selectedVariation.value.salePrice > 0 ? selectedVariation.value.salePrice : selectedVariation.value.price).toString();
+  }
 
   /// -- Check Product Variation Stock Status
-  void getProductVariationStatus(){
+  void getProductVariationStockStatus(){
     variationStockStatus.value = selectedVariation.value.stock > 0 ? 'In Stock' : 'Out of Stock';
   }
 
--- lib/features/shop/screens/product_details/widgets/product_attributes.dart
+++ lib/features/shop/screens/product_details/widgets/product_attributes.dart
@@ -2,10 +2,13 @@ import 'package:ecom_store/common/widgets/custom_shapes/containers/rounded_conta
 import 'package:ecom_store/common/widgets/products/product_cards/product_price.dart';
 import 'package:ecom_store/common/widgets/texts/product_title_text.dart';
 import 'package:ecom_store/common/widgets/texts/section_heading.dart';
+import 'package:ecom_store/features/shop/controllers/product/variation_controller.dart';
 import 'package:ecom_store/utils/constants/colors.dart';
 import 'package:ecom_store/utils/constants/sizes.dart';
 import 'package:ecom_store/utils/helpers/helper_functions.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:get/get_core/src/get_main.dart';
 
 import '../../../../../common/widgets/chips/choice_chip.dart';
 import '../../../models/product_model.dart';
@@ -17,94 +20,119 @@ class ecomProductAttributes extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-
     final dark = ecomHelperFunctions.isDarkMode(context);
-    final
+    final controller = Get.put(VariationController());
 
-    return Column(
-      children: [
-        /// -- Selected Attribute Pricing and Description
-        ecomRoundedContainer(
-          padding: const EdgeInsets.all(ecomSizes.medium),
-          bgColor: dark ? ecomColors.darkerGreyColor : ecomColors.greyColor,
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              /// Title, Price and Availability
-              Row(
+    return Obx(
+        () => Column(
+        children: [
+          /// -- Selected Attribute Pricing and Description
+          // Display variation price and stock when some variation is selected
+          if (controller.selectedVariation.value.id.isNotEmpty)
+            ecomRoundedContainer(
+              padding: const EdgeInsets.all(ecomSizes.medium),
+              bgColor: dark ? ecomColors.darkerGreyColor : ecomColors.greyColor,
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
-                  const ecomSectionHeading(
-                      title: "Variation", showActionButton: false),
-                  const SizedBox(width: ecomSizes.spaceBtwnItems),
-                  Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
+                  /// Title, Price and Availability
+                  Row(
                     children: [
-                      Row(
+                      const ecomSectionHeading(
+                          title: "Variation", showActionButton: false),
+                      const SizedBox(width: ecomSizes.spaceBtwnItems),
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
                         children: [
-                          const ecomProductTitleText(
-                              title: "Price : ", smallSize: true),
+                          Row(
+                            children: [
+                              const ecomProductTitleText(
+                                  title: "Price : ", smallSize: true),
 
-                          ///Actual price
+                              ///Actual price
+                              if(controller.selectedVariation.value.salePrice > 0)
+                              Text(
+                                "\$${controller.selectedVariation.value.price}",
+                                style: Theme.of(context)
+                                    .textTheme
+                                    .titleSmall!
+                                    .apply(
+                                        decoration: TextDecoration.lineThrough),
+                              ),
 
-                          Text(
-                            "\$25",
-                            style: Theme.of(context)
-                                .textTheme
-                                .titleSmall!
-                                .apply(decoration: TextDecoration.lineThrough),
+                              const SizedBox(width: ecomSizes.spaceBtwnItems),
+                              /// Sale Price
+                               ecomProductPriceText(price: controller.getVariationPrice()),
+                            ],
                           ),
 
-                          const SizedBox(width: ecomSizes.spaceBtwnItems),
-
-                          const ecomProductPriceText(price: "20"),
-                        ],
-                      ),
-                      Row(
-                        children: [
-                          const ecomProductTitleText(
-                              title: "Stock : ", smallSize: true),
-                          Text("In Stock",
-                              style: Theme.of(context).textTheme.titleMedium),
+                          /// Stock Status
+                          Row(
+                            children: [
+                              const ecomProductTitleText(
+                                  title: "Stock : ", smallSize: true),
+                              Text(controller.variationStockStatus.value,
+                                  style: Theme.of(context).textTheme.titleMedium!.apply(color: controller.variationStockStatus.value == 'In Stock' ? Colors.green : Colors.red)),
+                            ],
+                          )
                         ],
                       )
                     ],
-                  )
+                  ),
+
+                  /// Variation Description
+                  ecomProductTitleText(
+                      title: controller.selectedVariation.value.description ?? '',
+                      smallSize: true,
+                      maxLines: 4),
                 ],
               ),
+            ),
+          const SizedBox(height: ecomSizes.spaceBtwnItems),
 
-              /// Variation Description
-              const ecomProductTitleText(
-                  title:
-                      "This is the Description of the Product and it can go upto maximum 4 lines",
-                  smallSize: true,
-                  maxLines: 4),
-            ],
+          /// -- Attributes
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: product.productAttributes!
+                .map((attribute) => Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        ecomSectionHeading(
+                            title: attribute.name ?? '', showActionButton: false),
+                        const SizedBox(height: ecomSizes.spaceBtwnItems / 2),
+                        Obx(
+                          () => Wrap(
+                              spacing: 8,
+                              children: attribute.values!.map((attributeValue) {
+                                final isSelected =
+                                    controller.selectedAttributes[attribute.name] ==
+                                        attributeValue;
+                                final available = controller
+                                    .getAttributeAvailabilityInVariation(
+                                        product.productVariations!, attribute.name!)
+                                    .contains(attributeValue);
+
+                                return ecomChoiceChip(
+                                    text: attributeValue,
+                                    selected: isSelected,
+                                    onSelected: available
+                                        ? (selected) {
+                                            if (selected && available) {
+                                              controller.onAttributeSelected(
+                                                  product,
+                                                  attribute.name ?? '',
+                                                  attributeValue);
+                                            }
+                                          }
+                                        : null);
+                              }).toList()),
+                        )
+                      ],
+                    ))
+                .toList(),
           ),
-        ),
-        const SizedBox(height: ecomSizes.spaceBtwnItems),
-
-        /// -- Attributes
-        Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: product.productAttributes!
-              .map((attribute) => Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                       ecomSectionHeading(
-                          title: attribute.name ?? '', showActionButton: false),
-                      const SizedBox(height: ecomSizes.spaceBtwnItems / 2),
-                      Wrap(
-                        spacing: 8,
-                        children: attribute.values!.map((value) => ecomChoiceChip(
-                            text: value,
-                            selected: false,
-                            onSelected: (value) {})).toList(),
-                      ),
-                    ],
-                  ))
-              .toList(),
-        ),
-      ],
+        ],
+      ),
     );
   }
 }
